34646ba996f0dea853a8bfafcdb4d245309b2208
SirNoob97 <martinald467@gmail.com>
2021-09-29 21:53:20 +0200

    remove

--- src/test/java/com/github/sirnoob97/jdup/AppTest.java
+++ src/test/java/com/github/sirnoob97/jdup/AppTest.java
@@ -11,7 +11,5 @@ class AppTest {
   
   @Test
   void appHasAGreeting() {
-    App classUnderTest = new App();
-    assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
   }
 }
