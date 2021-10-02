f33649843ab91e25c11d3d72620388f9d856cd2b
SirNoob97 <martinald467@gmail.com>
2021-10-02 21:27:28 +0200

    add empty files option
    add method to return predicate according the empty files option
    
    unnecessary
    
    replace fields for getters and call the empty files predicate

--- src/main/java/com/github/sirnoob97/jdup/App.java
+++ src/main/java/com/github/sirnoob97/jdup/App.java
@@ -3,24 +3,21 @@
  */
 package com.github.sirnoob97.jdup;
 
+import com.github.sirnoob97.jdup.cli.JDupOption;
 import picocli.CommandLine;
 
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.Predicate;
 
 public class App {
 
-  private static final String EMPTY_FILE_HASH = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
-
   public static void main(String[] args) {
     JDupOption options = new JDupOption();
     new CommandLine(options).parseArgs(args);
-    var path = options.path;
+    var path = options.getPath();
 
     if (!Files.isDirectory(path) || !Files.exists(path)) {
       System.err.println("The path must be an existing directory!!");
@@ -29,14 +26,16 @@ public class App {
     var files = Visitor.visitRootDir(visitor ->
         visitor.root(path)
             .files(new HashSet<>())
-            .ignore(options.ignore));
+            .ignore(options.getIgnore()));
 
     Sha256Table.getDups(files)
         .entrySet().stream()
         .filter(entry -> entry.getValue().size() > 1)
+        .filter(options.getEmptyFilesPredicate())
         .peek(entry -> System.out.format("%nFiles that share the hash: %s%n", entry.getKey()))
         .map(Map.Entry::getValue)
         .flatMap(List::stream)
         .forEach(System.out::println);
   }
+
 }
--- src/main/java/com/github/sirnoob97/jdup/cli/JDupOption.java
+++ src/main/java/com/github/sirnoob97/jdup/cli/JDupOption.java
@@ -14,11 +14,18 @@ import java.util.function.Predicate;
 @Command
 public class JDupOption {
 
+  private static final String EMPTY_FILE_HASH = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+  private static final Predicate<Map.Entry<String, List<String>>> allowEmptyFiles = entry -> entry.getKey().equals(EMPTY_FILE_HASH);
+  private static final Predicate<Map.Entry<String, List<String>>> denyEmptyFiles = entry -> !entry.getKey().equals(EMPTY_FILE_HASH);
+
   @Parameters(defaultValue = "${user.dir}", description = "Path to check for duplicates, default: ${DEFAULT_VALUE}.")
   private Path path;
 
   @Option(names = { "-i", "--ignore" }, arity = "0..1", split = ",", description = "Comma separated list of directories to ignore.")
   private Set<String> ignore = new HashSet<>();
+
+  @Option(names = { "-e", "--empty" }, description = "Only show empty files.")
+  private boolean emptyFiles;
 
   public Path getPath() {
     return path;
@@ -26,5 +33,9 @@ public class JDupOption {
 
   public Set<String> getIgnore() {
     return ignore;
+  }
+
+  public Predicate<Map.Entry<String, List<String>>> getEmptyFilesPredicate() {
+    return emptyFiles ? allowEmptyFiles : denyEmptyFiles;
   }
 }
