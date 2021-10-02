384458db14f402bbeabfbc038f7878a84a7b3d01
SirNoob97 <martinald467@gmail.com>
2021-10-02 21:26:52 +0200

    format

--- src/main/java/com/github/sirnoob97/jdup/cli/JDupOption.java
+++ src/main/java/com/github/sirnoob97/jdup/cli/JDupOption.java
@@ -14,17 +14,17 @@ import java.util.function.Predicate;
 @Command
 public class JDupOption {
 
-    @Parameters(defaultValue = "${user.dir}", description = "Path to check for duplicates, default: ${DEFAULT_VALUE}.")
-    private Path path;
+  @Parameters(defaultValue = "${user.dir}", description = "Path to check for duplicates, default: ${DEFAULT_VALUE}.")
+  private Path path;
 
-    @Option(names = {"-i", "--ignore"}, arity = "0..1", split = ",", description = "Comma separated list of directories to ignore.")
-    private Set<String> ignore = new HashSet<>();
-    
-    public Path getPath() {
-        return path;
-    }
+  @Option(names = { "-i", "--ignore" }, arity = "0..1", split = ",", description = "Comma separated list of directories to ignore.")
+  private Set<String> ignore = new HashSet<>();
 
-    public Set<String> getIgnore() {
-        return ignore;
-    }
+  public Path getPath() {
+    return path;
+  }
+
+  public Set<String> getIgnore() {
+    return ignore;
+  }
 }
