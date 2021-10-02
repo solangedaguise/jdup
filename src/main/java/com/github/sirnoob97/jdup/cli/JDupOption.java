f721a943880f16cb9df5fb01b1d9da906e01309c
SirNoob97 <martinald467@gmail.com>
2021-10-02 22:11:01 +0200

    add custom help message

--- src/main/java/com/github/sirnoob97/jdup/cli/JDupOption.java
+++ src/main/java/com/github/sirnoob97/jdup/cli/JDupOption.java
@@ -11,6 +11,14 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.Predicate;
 
+@Command(headerHeading = "Usage:%n%n",
+    header = "Duplicate files finder.",
+    synopsisHeading = "%n",
+    parameterListHeading = "%nParameters:%n%n",
+    optionListHeading = "%nOptions%n%n",
+    descriptionHeading = "%nDescription:%n%n",
+    description = "Find duplicate files based on SHA256 hash, file name is ignored.",
+    abbreviateSynopsis = true)
 public class JDupOption {
 
   private static final String EMPTY_FILE_HASH = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
@@ -29,6 +37,9 @@ public class JDupOption {
 
   @Option(names = { "-e", "--empty" }, description = "Only show empty files.")
   private boolean emptyFiles;
+
+  @Option(names = { "-h", "--help" }, usageHelp = true, description = "display this help message")
+  boolean usageHelpRequested;
 
   public Path getPath() {
     return path;
