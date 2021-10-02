484efc841c1bdf97c2f69fe7639e825931dac8c6
SirNoob97 <martinald467@gmail.com>
2021-10-02 22:10:52 +0200

    format

--- src/main/java/com/github/sirnoob97/jdup/cli/JDupOption.java
+++ src/main/java/com/github/sirnoob97/jdup/cli/JDupOption.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.Predicate;
 
-@Command
 public class JDupOption {
 
   private static final String EMPTY_FILE_HASH = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
@@ -21,7 +20,11 @@ public class JDupOption {
   @Parameters(defaultValue = "${user.dir}", description = "Path to check for duplicates, default: ${DEFAULT_VALUE}.")
   private Path path;
 
-  @Option(names = { "-i", "--ignore" }, arity = "0..1", split = ",", description = "Comma separated list of directories to ignore.")
+  @Option(names = { "-i", "--ignore" },
+      arity = "0..1",
+      split = ",",
+      paramLabel = "DIR",
+      description = "Comma separated list of directories to ignore.")
   private Set<String> ignore = new HashSet<>();
 
   @Option(names = { "-e", "--empty" }, description = "Only show empty files.")
