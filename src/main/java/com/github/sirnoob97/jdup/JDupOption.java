f1e324d4063da5ef66e148b500023a1c39d0b3b4
SirNoob97 <martinald467@gmail.com>
2021-10-02 19:33:21 +0200

    add ignore option

--- src/main/java/com/github/sirnoob97/jdup/JDupOption.java
+++ src/main/java/com/github/sirnoob97/jdup/JDupOption.java
@@ -2,12 +2,18 @@ package com.github.sirnoob97.jdup;
 
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
 
 import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Set;
 
 @Command
 public class JDupOption {
 
     @Parameters(defaultValue = "${user.dir}", description = "Path to check for duplicates, default: ${DEFAULT_VALUE}.")
     Path path;
+
+    @Option(names = {"-i", "--ignore"}, arity = "0..1", split = ",", description = "Comma separated list of directories to ignore.")
+    Set<String> ignore = new HashSet<>();
 }
